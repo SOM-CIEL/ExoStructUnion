7c3f67f9edebf4b47f444318b2a7483a02e9385e
Nico Buldo <sombenicolas@gmail.com>
2024-11-25 16:55:57 +0100

    Commite#3

--- Transgerbeur/Source.cpp
+++ Transgerbeur/Source.cpp
@@ -3,47 +3,68 @@
 #include <iostream>
 using namespace std;
 
+struct SRouleau
+{
+	float longeur;
+	int numero;
+};
+
+struct TabCasier
+{
+	int SCasier[10] = { 0,1,2,3,4,5,6,7,8,9 };
+};
+struct SPalette
+{
+	int poids;
+	char reference[20];
+};
+
+union Ucontenu
+{
+	SPalette paletteCasier;
+	SRouleau rouleauCasier;
+
+};
+struct SCasier
+{
+	int py;
+	int pX;
+	Ucontenu contenu;
+};
+enum EEtat
+{
+	vide = 0,
+	palette = 1,
+	rouleau = 2,
+};
 
 int main()
 {
 	cout << "Trangerbeur" << endl;
 
 	
-	
+	SRouleau ro1 = { 10.5,552 };
+	SPalette pa1;
+	pa1.poids = 101.2;
+	strcpy_s(pa1.reference, "ref1");
 
-	union Ucontenu
-	{
-		struct palette;
-		struct roueau;
+	int etatC1 = vide;
 
-	};
-	enum EEtat
-	{
-		vide = 0,
-		palette = 1,
-		rouleau = 2,
-	};
+	Ucontenu cont = { pa1 };
 
-	struct Srouleau
-	{
-		float longeur;
-		int numero;
-	};
-	struct casier
-	{
-		
-		int py;
-		int pi;
-	};
-	struct TabCasier
-	{
-		int SCasier[10] = { 0,1,2,3,4,5,6,7,8,9 };
-	};
-	struct SPalette
-	{
-		int poids;
-		char reference[19];
-	};
+	cont.rouleauCasier = ro1;
+
+	SCasier c1;
+	c1.contenu.paletteCasier = pa1;
+
+	 
+	SCasier c2;
+	c2.contenu.paletteCasier = pa1;
+
+	SCasier TCasier[2];
+	TCasier[0] = c1;
+	TCasier[2] = c2;
+
 
 
 
