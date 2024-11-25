8034c31f8bbfd0d5308977865bf1a54d5acfa824
Nico Buldo <sombenicolas@gmail.com>
2024-11-25 16:00:33 +0100

    Commite#2

--- Transgerbeur/Source.cpp
+++ Transgerbeur/Source.cpp
@@ -8,4 +8,43 @@ int main()
 {
 	cout << "Trangerbeur" << endl;
 
+	
+	
+
+	union Ucontenu
+	{
+		struct palette;
+		struct roueau;
+
+	};
+	enum EEtat
+	{
+		vide = 0,
+		palette = 1,
+		rouleau = 2,
+	};
+
+	struct Srouleau
+	{
+		float longeur;
+		int numero;
+	};
+	struct casier
+	{
+		
+		int py;
+		int pi;
+	};
+	struct TabCasier
+	{
+		int SCasier[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	};
+	struct SPalette
+	{
+		int poids;
+		char reference[19];
+	};
+
+
+
 }
